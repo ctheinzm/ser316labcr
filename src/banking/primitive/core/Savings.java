7c81e9d483189e2837437c3b03a1ee332ce196c6
gi1023839468 <hzhan147@asu.edu>
2017-02-20 22:19:30 -0700

    Fixed banners and naming conventions issue #29.

--- src/banking/primitive/core/Savings.java
+++ src/banking/primitive/core/Savings.java
@@ -1,20 +1,61 @@
+/*
+  File:	Savings.java
+  Author: Professor Mehlhase, Fixed by Haocheng Zhang
+  Date:	2/20/17
+  
+  Description: The class is to handle Savings account.
+*/
 package banking.primitive.core;
 
-public class Savings extends Account {
-	private static final long serialVersionUID = 111L;
-	private int numWithdraws = 0;
+/**
+Class:	Savings
 
+Description: The class is a special account savings account and have multiple methods to access savings account.
+*/
+public class Savings extends Account {
+
+	/**
+	  Method: Savings(String name) 
+	  Inputs: name
+	  Returns: 
+	
+	  Description: The method construct a new account with name.
+	*/
 	public Savings(String name) {
 		super(name);
 	}
 
+	/**
+	  Method: Savings(String name, float balance)
+	  Inputs: name and balance
+	  Returns: 
+	
+	  Description: The method construct a new account with name and balance.
+	*/
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
 
 	/**
-	 * A deposit comes with a fee of 50 cents per deposit
-	 */
+	  Method: getType() 
+	  Inputs: 
+	  Returns: String "Savings"
+	
+	  Description: The method return a string "Savings".
+	*/
+	public String getType() { return "Savings"; }
+
+	
+	
+	/**
+	  Method: deposit(float amount) 
+	  Inputs: amount
+	  Returns: boolean
+	
+	  Description: The method deposit money into account by specific amount.
+	  			   A deposit may be made unless the Savings account is closed
+	  			   A deposit comes with a fee of 50 cents per deposit
+	*/
 	public boolean deposit(float amount) {
 		if (getState() != State.CLOSED && amount > 0.0f) {
 			balance = balance + amount - 0.50F;
@@ -26,15 +67,21 @@ public class Savings extends Account {
 		return false;
 	}
 
+	
 	/**
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
+	  Method: withdraw(float amount) 
+	  Inputs: amount
+	  Returns: boolean
+	
+	  Description: The method withdraw money from account by specific amount.
+	  			   After 3 withdrawals a fee of $1 is added to each withdrawal.
+	  			   An account whose balance dips below 0 is in an OVERDRAWN state
+	*/
 	public boolean withdraw(float amount) {
 		if (getState() == State.OPEN && amount > 0.0f) {
 			balance = balance - amount;
-			numWithdraws++;
-			if (numWithdraws > 3)
+			_numWithdraws++;
+			if (_numWithdraws > 3)
 				balance = balance - 1.0f;
 			// KG BVA: should be < 0
 			if (balance < 0.0f) {
@@ -44,10 +91,18 @@ public class Savings extends Account {
 		}
 		return false;
 	}
-	
-	public String getType() { return "Savings"; }
 
+	/**
+	  Method: toString() 
+	  Inputs: 
+	  Returns: String "Savings" + name + balance
+	
+	  Description: The method print the savings account infos.
+	*/
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
 	}
+	
+	private static final long serialVersionUID = 111L;
+	private int _numWithdraws = 0;
 }
