0803b984e7e8fca171cd5299b285279a09c68e38
gi1023839468 <hzhan147@asu.edu>
2017-02-20 21:48:29 -0700

    Fixed banner and private naming conventions issue #22.

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -1,26 +1,66 @@
+/*
+  File:	Checking.java
+  Author: Professor Mehlhase, Fixed by Haocheng Zhang
+  Date:	2/20/17
+  
+  Description: The class is to handle checking account.
+*/
+
 package banking.primitive.core;
 
+/**
+Class:	Checking
+
+Description: The class is a special account checking account and have multiple methods to access checking account.
+*/
 public class Checking extends Account {
 
 	private static final long serialVersionUID = 11L;
-	private int numWithdraws = 0;
+	private int _numWithdraws = 0;
+
+	/**
+	  Method: Checking(String name) 
+	  Inputs: name
+	  Returns: 
 	
+	  Description: The method construct a new account with name.
+	*/
 	private Checking(String name) {
 		super(name);
 	}
 
-    public static Checking createChecking(String name) {
-        return new Checking(name);
-    }
-
+	/**
+	  Method: Checking(String name) 
+	  Inputs: name and balance
+	  Returns: 
+	
+	  Description: The method construct a new account with name and balance.
+	*/
 	public Checking(String name, float balance) {
 		super(name, balance);
 	}
 
+	
 	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
+	  Method: createChecking(String name) 
+	  Inputs: name
+	  Returns: The method return a new checking account with name.
+	
+	  Description: The method create a new checking account with name.
+	*/
+  public static Checking createChecking(String name) {
+      return new Checking(name);
+  }
+  
+  
+	/**
+	  Method: deposit(float amount) 
+	  Inputs: amount
+	  Returns: boolean
+	
+	  Description: The method deposit money into account by specific amount.
+	  			   A deposit may be made unless the Checking account is closed
+	*/
 	public boolean deposit(float amount) {
 		if (getState() != State.CLOSED && amount > 0.0f) {
 			balance = balance + amount;
@@ -31,18 +71,24 @@ public class Checking extends Account {
 		}
 		return false;
 	}
-
+	
+	
 	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
+	  Method: withdraw(float amount) 
+	  Inputs: amount
+	  Returns: boolean
+	
+	  Description: The method withdraw money from account by specific amount.
+	  			   After 10 withdrawals a fee of $2 is charged per transaction You may
+	  			   continue to withdraw an overdrawn account until the balance is below -$100
+	*/
 	public boolean withdraw(float amount) {
 		if (amount > 0.0f) {		
 			// KG: incorrect, last balance check should be >=
 			if (getState() == State.OPEN || (getState() == State.OVERDRAWN && balance > -100.0f)) {
 				balance = balance - amount;
-				numWithdraws++;
-				if (numWithdraws > 10)
+				_numWithdraws++;
+				if (_numWithdraws > 10)
 					balance = balance - 2.0f;
 				if (balance < 0.0f) {
 					setState(State.OVERDRAWN);
@@ -53,8 +99,23 @@ public class Checking extends Account {
 		return false;
 	}
 
+	/**
+	  Method: getType() 
+	  Inputs: 
+	  Returns: String "checking"
+	
+	  Description: The method return a string "Checking".
+	*/
 	public String getType() { return "Checking"; }
 	
+	
+	/**
+	  Method: toString() 
+	  Inputs: 
+	  Returns: String "checking" + name + balance
+	
+	  Description: The method print the checking account infos.
+	*/
 	public String toString() {
 		return "Checking: " + getName() + ": " + getBalance();
 	}
